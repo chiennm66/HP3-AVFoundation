1fbacb35e549c5f6da38ea2806e8a71f8ed78986
chienabc <chien97666@gmail.com>
2025-06-14 16:44:12 +0700

    Initial Commit

---
+++ LESSON1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LESSON1
+//
+//  Created by CHIEN on 14/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
